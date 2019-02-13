b260550c992582146ad7d4fce86c030ae8c32bb8
Kat B <katreesebarnes@gmail.com>
2019-02-13 17:56:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(sentence1)
   new_array = []
   new_sentence = sentence1.split
   
-   new_sentence.reverse_each  {|word|new_array << word }
+   new_sentence.reverse_each  {|word| new_array << word }
   
-   #new_array  << word
+   
    
    new_array.join()
 
