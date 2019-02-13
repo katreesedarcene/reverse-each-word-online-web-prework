08ad85a63d257491e0612333c542ada1d02a87d6
Kat B <katreesebarnes@gmail.com>
2019-02-13 17:35:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,11 @@
 def reverse_each_word(sentence1)
-  
+  new_array = []
   new_sentence = sentence1.split
   
    new_sentence.reverse_each  {|word| word }
   
-   new_sentence.push
+   new_sentence << new_array
 
-     
-
-  
  end
  
 
